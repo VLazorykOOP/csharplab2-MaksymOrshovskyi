bfde074282e13f93a4b388cacff1d804e0142659
Orshovskyi Maksym <137319202+MaksymOrshovskyi@users.noreply.github.com>
2024-04-21 19:11:08 +0300

    add task 2

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -29,5 +29,47 @@ class Program
         {
             Console.Write(item + " ");
         }
+
+        Console.WriteLine();
+//_____________________________________________________________________
+        // Введення розміру масиву
+        Console.WriteLine("Введіть розмір масиву:");
+        int n = int.Parse(Console.ReadLine());
+
+        // Ініціалізація масиву з введеним розміром
+        double[] array2 = new double[n];
+
+        // Зчитування елементів масиву з клавіатури
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Введіть {0}-й елемент масиву:", i + 1);
+            array2[i] = double.Parse(Console.ReadLine());
+        }
+
+        // Знаходження максимального значення в масиві
+        double max = array2[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (array2[i] > max)
+            {
+                max = array2[i];
+            }
+        }
+
+        // Заміна всіх максимальних елементів нулями
+        for (int i = 0; i < n; i++)
+        {
+            if (array2[i] == max)
+            {
+                array2[i] = 0;
+            }
+        }
+
+        // Виведення зміненого масиву
+        Console.WriteLine("Масив після заміни:");
+        foreach (var item in array2)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
